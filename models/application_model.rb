3106c316173659867d5cb92bfb1e7b400315989e
Hansford Nguyen <hansfordnguyen@hotmail.com>
2019-12-02 19:41:13 +0700

    MW-5 Update application model

--- models/application_model.rb
+++ models/application_model.rb
@@ -1,2 +1,11 @@
+require 'yaml'
+
 class ApplicationModel
+  def self.table_name
+    underscore(self.name)
+  end
+
+  def self.all
+    YAML.load_file("#{APP_ROOT}/database/#{table_name}.yml")
+  end
 end
---
+++ spec/models/application_model_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../models/application_model'
+
+describe ApplicationModel do
+  class MyModel < ApplicationModel; end
+
+  describe '.table_name' do
+    subject { MyModel.table_name }
+
+    it { is_expected.to eq 'my_model' }
+  end
+
+  describe '.all' do
+    subject { MyModel.all }
+
+    before do
+      APP_ROOT = 'APP_ROOT'
+      allow(YAML).to receive(:load_file).and_return('raw data')
+    end
+
+    it 'loads correct data' do
+      subject
+      expect(YAML).to have_received(:load_file).with('APP_ROOT/database/my_model.yml')
+    end
+
+    it { is_expected.to eq 'raw data' }
+  end
+end
