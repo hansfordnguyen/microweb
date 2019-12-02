f98d895281034bfa5193b46e8a33c7fb8731c388
Hansford Nguyen <hansfordnguyen@hotmail.com>
2019-12-02 19:39:57 +0700

    MW-5 Add global helpers

--- config/route.rb
+++ config/route.rb
@@ -3,4 +3,5 @@ require 'router'
 Router.draw do
   Router.get 'home', to: 'home#index'
   Router.get 'about', to: 'home#about'
+  Router.get 'users', to: 'users#index'
 end
--- init.rb
+++ init.rb
@@ -8,5 +8,8 @@ $:.unshift( File.join(APP_ROOT, 'views') )
 $:.unshift( File.join(APP_ROOT, 'database') )
 
 require "#{APP_ROOT}/server"
+require "#{APP_ROOT}/core/global_helpers"
+
+include GlobalHelpers
 
 application = Server.new.start
